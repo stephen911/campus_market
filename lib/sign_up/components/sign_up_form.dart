ad15fba284e03f90fb1af929237f7e746a65819e
daniel <adanielagyei@gmail.com>
2022-08-12 04:29:10 +0000

    fully done with firebase sign in and sign up

--- lib/sign_up/components/sign_up_form.dart
+++ lib/sign_up/components/sign_up_form.dart
@@ -30,6 +30,9 @@ class _SignUpFormState extends State<SignUpForm> {
   // String? password;
   // String? conform_password;
   // String? contact;
+  bool isObscured = false;
+  bool isObscured1 = false;
+
   final _auth = FirebaseAuth.instance;
 
   String? errorMessage;
@@ -56,175 +59,251 @@ class _SignUpFormState extends State<SignUpForm> {
   @override
   Widget build(BuildContext context) {
     TextFormField buildConformPassFormField() {
-    return TextFormField(
-      controller: confirmPassController,
-      obscureText: true,
-      onSaved: (newValue) => confirmPassController.text = newValue!,
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kPassNullError);
-        } else if (value.isNotEmpty &&
-            passwordController.text == confirmPassController.text) {
-          removeError(error: kMatchPassError);
-        }
-        confirmPassController.text = value;
-      },
-      validator: (value) {
-        if (value!.isEmpty) {
-          addError(error: kPassNullError);
-          return "";
-        } else if ((passwordController.text != value)) {
-          addError(error: kMatchPassError);
-          return "";
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-        labelText: "Confirm Password",
-        hintText: "Re-enter your password",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Lock.svg"),
-      ),
-    );
-  }
-
-  TextFormField buildPasswordFormField() {
-    return TextFormField(
-      controller: passwordController,
-      obscureText: true,
-      onSaved: (newValue) => passwordController.text = newValue!,
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kPassNullError);
-        } else if (value.length >= 6) {
-          removeError(error: kShortPassError);
-        }
-        passwordController.text = value;
-      },
-      validator: (value) {
-        if (value!.isEmpty) {
-          addError(error: kPassNullError);
-          return "";
-        } else if (value.length < 5) {
-          addError(error: kShortPassError);
-          return "";
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-        labelText: "Password",
-        hintText: "Enter your password",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Lock.svg"),
-      ),
-    );
-  }
-
-  TextFormField buildEmailFormField() {
-    return TextFormField(
-      controller: emailController,
-      keyboardType: TextInputType.emailAddress,
-      onSaved: (newValue) => emailController.text = newValue!,
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kEmailNullError);
-        } else if (emailValidatorRegExp.hasMatch(value)) {
-          removeError(error: kInvalidEmailError);
-        }
-        return null;
-      },
-      validator: (value) {
-        if (value!.isEmpty) {
-          addError(error: kEmailNullError);
-          return "";
-        } else if (!emailValidatorRegExp.hasMatch(value)) {
-          addError(error: kInvalidEmailError);
-          return "";
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-        labelText: "Email",
-        hintText: "Enter your email",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
-      ),
-    );
-  }
-
-  TextFormField buildContactField() {
-    return TextFormField(
-      keyboardType: TextInputType.number,
-      controller: phoneController,
-      obscureText: true,
-      onSaved: (newValue) => phoneController.text = newValue!,
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kPhoneNumberNullError);
-        } else if (value.isNotEmpty) {
-          removeError(error: kPhoneNumberNullError);
-        }
-        phoneController.text = value;
-      },
-      validator: (value) {
-        if (value!.isEmpty) {
-          addError(error: kPhoneNumberNullError);
-          return "";
-        } else if ((phoneController.text != value)) {
-          addError(error: kPhoneNumberNullError);
-          return "";
-        } else if (value.length < 10) {
-          addError(error: kShortPhone);
-        } else if (value.length > 10) {
-          addError(error: kLongPhone);
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-        labelText: "Enter phone number",
-        hintText: "Phone number",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: Icon(
-          Icons.phone,
+      return TextFormField(
+        obscureText: isObscured1,
+        controller: confirmPassController,
+        onSaved: (val) {
+          confirmPassController.text = val!;
+        },
+        validator: (value) {
+          if (value!.isEmpty) {
+            // return 'Please enter password';
+            addError(error: kPassNullError);
+            return "";
+          } else if (confirmPassController.text != passwordController.text) {
+            // return 'Passwords doesn\'t match';
+            addError(error: kMatchPassError);
+            return "";
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          labelText: "password",
+          hintText: 'Confirm Password',
+          prefixIcon: const Icon(
+            Icons.vpn_key,
+            size: 20,
+          ),
+          suffixIcon: GestureDetector(
+            onTap: () {
+              setState(() {
+                isObscured1 = !isObscured1;
+              });
+            },
+            child: Icon(
+              isObscured ? Icons.remove_red_eye : Icons.remove_red_eye_outlined,
+            ),
+          ),
         ),
-      ),
-    );
-  }
+      );
+      // TextFormField(
+      //   controller: confirmPassController,
+      //   obscureText: true,
+      //   onSaved: (newValue) => confirmPassController.text = newValue!,
+      //   onChanged: (value) {
+      //     if (value.isNotEmpty) {
+      //       removeError(error: kPassNullError);
+      //     } else if (value.isNotEmpty &&
+      //         passwordController.text == confirmPassController.text) {
+      //       removeError(error: kMatchPassError);
+      //     }
+      //     confirmPassController.text = value;
+      //   },
+      //   validator: (value) {
+      //     if (value!.isEmpty) {
+      //       addError(error: kPassNullError);
+      //       return "";
+      //     } else if ((passwordController.text != value)) {
+      //       addError(error: kMatchPassError);
+      //       return "";
+      //     }
+      //     return null;
+      //   },
+      //   decoration: InputDecoration(
+      //     labelText: "Confirm Password",
+      //     hintText: "Re-enter your password",
+      //     floatingLabelBehavior: FloatingLabelBehavior.always,
+      //     suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Lock.svg"),
+      //   ),
+      // );
+    }
 
-  TextFormField buildNameField() {
-    return TextFormField(
-      keyboardType: TextInputType.number,
-      controller: nameController,
-      obscureText: true,
-      onSaved: (newValue) => nameController.text = newValue!,
-      onChanged: (value) {
-        if (value.isNotEmpty) {
-          removeError(error: kNamelNullError);
-        } else if (value.isNotEmpty) {
-          removeError(error: kNamelNullError);
-        }
-        nameController.text = value;
-      },
-      validator: (value) {
-        if (value!.isEmpty) {
-          addError(error: kNamelNullError);
-          return "";
-        } else if ((nameController.text != value)) {
-          addError(error: kNamelNullError);
-          return "";
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-        labelText: "Full name",
-        hintText: "Enter Full Name",
-        floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: Icon(
-          Icons.person,
+    TextFormField buildPasswordFormField() {
+      return TextFormField(
+        obscureText: isObscured,
+        controller: passwordController,
+        onSaved: (val) {
+          passwordController.text = val!;
+        },
+        validator: (value) {
+          if (value!.isEmpty) {
+            // return 'Please enter password';
+            addError(error: kPassNullError);
+            return "";
+          }
+
+          RegExp regex = new RegExp(r'^.{6,}$');
+          if (value.isEmpty) {
+            addError(error: kPassNullError);
+            return "";
+            // return ("Password is required");
+          }
+          if (!regex.hasMatch(value)) {
+            // return ("Enter Valid Password(Min. 6 Character)");
+            addError(error: kShortPassError);
+            return "";
+          }
+          return null;
+        },
+        decoration: InputDecoration(
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: 'Enter your password',
+          labelText: "Password",
+          prefixIcon: const Icon(
+            Icons.vpn_key,
+            size: 20,
+          ),
+          suffixIcon: GestureDetector(
+            onTap: () {
+              setState(() {
+                isObscured = !isObscured;
+              });
+            },
+            child: Icon(
+              isObscured ? Icons.remove_red_eye : Icons.remove_red_eye_outlined,
+            ),
+          ),
         ),
-      ),
-    );
-  }
+      );
+      // TextFormField(
+      //   controller: passwordController,
+      //   obscureText: true,
+      //   onSaved: (newValue) => passwordController.text = newValue!,
+      //   onChanged: (value) {
+      //     if (value.isNotEmpty) {
+      //       removeError(error: kPassNullError);
+      //     } else if (value.length >= 6) {
+      //       removeError(error: kShortPassError);
+      //     }
+      //     passwordController.text = value;
+      //   },
+      //   validator: (value) {
+      //     if (value!.isEmpty) {
+      //       addError(error: kPassNullError);
+      //       return "";
+      //     } else if (value.length < 5) {
+      //       addError(error: kShortPassError);
+      //       return "";
+      //     }
+      //     return null;
+      //   },
+      //   decoration: InputDecoration(
+      //     labelText: "Password",
+      //     hintText: "Enter your password",
+      //     floatingLabelBehavior: FloatingLabelBehavior.always,
+      //     suffixIcon: CustomSurffixIcon(svgIcon: "assets/icons/Lock.svg"),
+      //   ),
+      // );
+    }
+
+    TextFormField buildEmailFormField() {
+      return TextFormField(
+        controller: emailController,
+        onSaved: (val) {
+          emailController.text = val!;
+        },
+        validator: (value) {
+          if (value!.isEmpty) {
+            addError(error: kEmailNullError);
+            return "";
+          }
+
+          if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+              .hasMatch(value)) {
+            // return ("Please Enter a valid email");
+            addError(error: kInvalidEmailError);
+            return "";
+          }
+          return null;
+        },
+        decoration: const InputDecoration(
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          labelText: "Enter Email",
+          hintText: 'Email',
+          // suffix: CustomSurffixIcon(svgIcon: "assets/icons/Mail.svg"),
+          prefixIcon: Icon(
+            Icons.email,
+            size: 20,
+          ),
+        ),
+      );
+      //
+    }
+
+    TextFormField buildContactField() {
+      return TextFormField(
+        keyboardType: TextInputType.number,
+        controller: phoneController,
+        onSaved: (val) {
+          phoneController.text = val!;
+        },
+        validator: (value) {
+          if (value!.isEmpty) {
+            // return 'Please enter phone number';
+            addError(error: kPhoneNumberNullError);
+            return "";
+          }
+          final max_length = 10;
+          if (value.length > max_length) {
+            // return "please phone number cannot be more than 10";
+            addError(error: kLongPhone);
+            return "";
+          } else if (value.length < 10) {
+            addError(error: kShortPhone);
+            return "";
+          }
+
+          return null;
+        },
+        decoration: const InputDecoration(
+          labelText: "Enter phone number",
+          hintText: "Phone number",
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          prefixIcon: Icon(
+            Icons.phone,
+            size: 20,
+          ),
+        ),
+      );
+    }
+
+    TextFormField buildNameField() {
+      return TextFormField(
+        controller: nameController,
+        onSaved: (val) {
+          nameController.text = val!;
+        },
+        validator: (value) {
+          if (value!.isEmpty) {
+            // return 'Please enter surname';
+            addError(error: kNamelNullError);
+            return "";
+          }
+          return null;
+        },
+        decoration: const InputDecoration(
+          floatingLabelBehavior: FloatingLabelBehavior.always,
+          hintText: 'Enter Full Name',
+          labelText: "Full Name",
+          prefixIcon: Icon(
+            Icons.person,
+            size: 20,
+          ),
+        ),
+      );
+    }
+
     return Form(
       key: _formKey,
       child: Column(
@@ -239,7 +318,6 @@ class _SignUpFormState extends State<SignUpForm> {
           SizedBox(height: 20),
           buildConformPassFormField(),
           SizedBox(height: 20),
-          SizedBox(height: 30),
           FormError(errors: errors),
           SizedBox(height: 20),
           DefaultButton(
@@ -331,6 +409,4 @@ class _SignUpFormState extends State<SignUpForm> {
       Fluttertoast.showToast(msg: errorMessage!);
     }
   }
-
-  
 }
