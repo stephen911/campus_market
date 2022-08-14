923fbdd130763ae2a802abc253ea9cde1933616b
daniel <adanielagyei@gmail.com>
2022-08-14 23:22:40 +0000

    building notification page and card

--- lib/screens/notification/notifications.dart
+++ lib/screens/notification/notifications.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../components/notification_card.dart';
+
 class Notifications extends StatefulWidget {
   const Notifications({Key? key}) : super(key: key);
 
@@ -7,15 +9,39 @@ class Notifications extends StatefulWidget {
   State<Notifications> createState() => _NotificationsState();
 }
 
+List<Color> randomColors = [
+  Colors.red,
+  Colors.orange,
+  Colors.green,
+  Colors.blue,
+  Colors.indigo,
+  Colors.yellow,
+  Colors.lightBlue
+];
+
 class _NotificationsState extends State<Notifications> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: EdgeInsets.all(10),
         color: Colors.green,
-        child: Center(
-          child: Text("notification goes here"),
-        ),
+        child: Column(children: [
+          for (int i = 0; i < 8; i++)
+            NotificationCard(
+              parentId: 'parentId',
+              message: 'your order has been approved',
+              title: 'order',
+              time: DateTime.now().toString(),
+              onTap: () {
+                print("card has been tapped");
+              },
+              opened: true,
+              date: DateTime.now().toString(),
+              color: randomColors[i % 7],
+              sender: ' Owner',
+            ),
+        ]),
       ),
     );
   }
